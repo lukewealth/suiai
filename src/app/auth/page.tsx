ab96d4b2c5f4763f373a181a6220748e95be3c14
Dxtobi <akanbijosephtobi@gmail.com>
2024-05-01 11:46:34 -0700

    UPDATED

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -7,11 +7,17 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+
+
+
+
+
 export default function Auth() {
   const router = useRouter();
   const session = useSession();
 
   const [current_auth_component_login, switchAuthComponent] = useState(false);
+
 
   function toggleAuthComponent() {
     switchAuthComponent(!current_auth_component_login);
@@ -24,28 +30,32 @@ export default function Auth() {
     }
   });
   return (
-    <div className='h-screen flex items-end '>
-      <div className='bg-secondary flex justify-center items-center  flex-col h-full w-[75%]'>
+
+    <div className="min-h-screen flex ">
+      <div className="bg-secondary flex justify-center items-center flex-col w-full">
+
         {current_auth_component_login ? (
           <Login changeAuth={toggleAuthComponent} />
         ) : (
           <Register changeAuth={toggleAuthComponent} />
         )}
       </div>
-      <div className=' bg-gradient-to-b to-[#E750FF] h-full from-[#9D1AFE] flex justify-center items-center flex-col w-[25%] text-white px-6'>
-        <div className='flex flex-col gap-8'>
+
+      <div className=" bg-gradient-to-b to-[#E750FF] from-[#9D1AFE] flex justify-center items-center flex-col w-[30%] text-white px-6">
+        <div className="flex flex-col gap-8">
           <Image
-            className=''
+            className=""
             src={"/images/ailogo.png"}
-            alt=''
+            alt=""
             width={150}
             height={100}
           />
-          <h1 className='text-[48px] '>
+          <h1 className="text-[48px] ">
             Learn, Discover & Automate in One Place.
           </h1>
-          <p className='text-sm w-[80%]'>
-            SUI AI using Move programming language to answer all your smart
+          <p>
+            SUI GPT using Move programming language to answer all your smart
+     
             contract questions!
           </p>
         </div>
