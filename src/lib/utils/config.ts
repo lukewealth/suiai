0e07b2f2aea29314664b7e35eb151d2c95a38362
lukewealth <lukemonry@gmail.com>
2024-05-19 11:00:12 +0100

    added base url for configuration

--- src/lib/utils/config.ts
+++ src/lib/utils/config.ts
@@ -1,5 +1,5 @@
 export const serverUrl = "https://rag-production-2b1c.up.railway.app";
 export const baseUrl =
   process.env.NODE_ENV == "production"
-    ? "https://.vercel.app"
+    ? "https://suiai.vercel.app"
     : "http://localhost:3000";
